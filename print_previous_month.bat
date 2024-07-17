a19ff743189ce38b4c159000886cbb02aa1d40fb
invalidusernamecalled <puneetbapna@gmail.com>
2024-07-17 19:54:07 +0530

    Add files via upload

--- print_previous_month.bat
+++ print_previous_month.bat
@@ -1,6 +1,11 @@
 @echo off & setlocal enabledelayedexpansion
 REM batch script to print month 
-set exitfaster=0
+
+if %1 LSS 20000 if %1 GTR -20000 goto next
+
+goto :eof
+
+:next
 REM reliably retrieve current date/time using wmic
 REM works across systems
 for /f "tokens=1 delims=." %%i in ('wmic os get localdatetime ^| findstr /r "[0-9]"') do set current_date=%%i
@@ -8,13 +13,12 @@ REM extract month from date, 4th to 6th character
 set current_month=%current_date:~4,2%
 
 REM ask user how long he wants to go back
-set exitfaster=1&set howmany=%1
-if "%2" == "+" (set operator_= + ) else (set operator_= - )
+set howmany=%1
 
 :print
 REM reliably get previous months number
 set /a mod=%howmany% %% 12
-set /a previous_month=current_month%operator_%mod
+set /a previous_month=current_month+mod
 if %previous_month%==0 set previous_month=12
 if %previous_month% LSS 0 set /a previous_month=12+previous_month
 
@@ -31,6 +35,5 @@ set /a index+=1
 ::FINALLY print the previous month
 echo !month_by_index[%previous_month%]!
 
-if %exitfaster% == 0 pause >NUL
 
 
