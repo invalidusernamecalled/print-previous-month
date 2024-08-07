609667ac65484339195cf96fbd0a0ad92156143c
invalidusernamecalled <puneetbapna@gmail.com>
2024-08-07 14:07:40 +0530

    Update print_previous_month.bat

--- print_previous_month.bat
+++ print_previous_month.bat
@@ -11,8 +11,7 @@ REM reliably retrieve current date/time using wmic
 REM works across systems
 for /f "tokens=1 delims=." %%i in ('wmic os get localdatetime ^| findstr /r "[0-9]"') do set current_date=%%i
 REM extract month from date, 4th to 6th character
-if "%current_date:~4,1%"=="0" (set current_month=%current_date:~5,1%
-) else (set current_month=%current_date:~4,2%)
+if "%current_date:~4,1%"=="0" (set current_month=%current_date:~5,1%) else (set current_month=%current_date:~4,2%)
 :print
 REM reliably get previous months number
 set mod=%howmany%
